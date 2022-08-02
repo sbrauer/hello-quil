13606fe04e4bfa0e0cf07a044144312f1acbbfc4
Sam Brauer <sam.brauer@gmail.com>
2022-08-01 20:32:45 -0400

    Tolerate leading # in hex color string

--- src/hello_quil/util.clj
+++ src/hello_quil/util.clj
@@ -1,11 +1,19 @@
-(ns util)
+(ns util
+  (:require [clojure.string :as str]))
 
 (defn hex->int
   [s]
   (Integer/parseInt s 16))
 
+(defn- strip-hash
+  [s]
+  (if (str/starts-with? s "#")
+    (subs s 1)
+    s))
+
 (defn hex->color
   [s]
-  [(hex->int  (subs s 0 2))
-   (hex->int  (subs s 2 4))
-   (hex->int  (subs s 4 6))])
+  (let [s (strip-hash s)]
+    [(hex->int  (subs s 0 2))
+     (hex->int  (subs s 2 4))
+     (hex->int  (subs s 4 6))]))
