a93ea20d0ac04f96744822aa005cd3ddbf8389a8
Sam Brauer <sam.brauer@gmail.com>
2022-08-04 14:21:02 -0400

    WIP

--- src/hello_quil/midi_note_viz3.clj
+++ src/hello_quil/midi_note_viz3.clj
@@ -1,13 +1,22 @@
 (ns hello-quil.core
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
-            [hello-quil.midi-util :as midi]))
+            [hello-quil.midi-util :as midi])
+  (:import java.time.Instant))
 
 ;; Ideally should be configurable instead of hard-coded
 (def midi-device-description "IAC Driver IAC Bus 1")
 
+(def window-seconds 4)
+(def window-microseconds (* 1000000 window-seconds))
+
 (def state-atom (atom {:midi-dev nil
-                       :notes {}}))
+                       :notes {}
+                       :first-midi-ts-micros nil
+                       :first-epoch-ts-millis nil}))
+
+(defn current-epoch-millis []
+  (inst-ms (Instant/now)))
 
 ;; https://colorswall.com/palette/3261
 (def diatonic-colors
@@ -46,30 +55,52 @@
     (q/background 0))
 
 (defn draw-note
-  [{:keys [channel key velocity] :as note}]
+  ;; FIXME: x and w should be based on start-ts and stop-ts relative to current-timestamp and window-microseconds
+  ;; Shit!  current-timestamp will only update when midi comes in.
+  ;; We need to change notion of time; maybe note the first midi timestamp we get
+  ;; as a base time, as well as our current timestamp in microseconds and normalize so that we can track time based on sys clock instead of midi.
+  [{:keys [key start-ts stop-ts] :as note} current-timestamp]
   ;;(println "draw-note" note)
   (q/fill (note-key->color key))
-  (let [h (q/height)
-        w (/ (q/width) 128)
-        y (- (q/height) h)
-        x (* key w)]
+  (let [h (/ (q/height) 128)
+        w :FIXME-based-on-time
+        y (* key h)
+        x :FIXME-based-on-time]
     (q/rect x y w h)))
 
 (defn draw []
   ;;(println "DRAW" @state-atom)
-  #_(let [{:keys [notes]} @state-atom]
-    (clear-screen)
-    (doseq [note (vals notes)]
-      (draw-note note))))
+  (clear-screen)
+  (let [{:keys [notes timestamp]} @state-atom
+        ;; FIXME: we need to account for more than one note per key!!!
+        ;; Also move these code into handle-midi-event so that
+        ;; we do most calculation on midi event instead of in draw.
+        notes-by-key (reduce (fn [acc {:keys [event ts] :as note}]
+                               (let [{:keys [command key]} event]
+                                 (case command
+                                   :on (assoc-in acc [key :start-ts] ts)
+                                   :off (assoc-in acc [key :stop-ts] ts)
+                                   nil)))
+                             {}
+                             notes)]
+    (doseq [[key note] notes]
+      ;;(draw-note (assoc note :key key) timestamp)
+      )))
 
 (defn handle-midi-event
   [event-info timestamp]
+  ;; Note first timestamps
+  (when-not (:first-midi-ts-micros @state-atom)
+    (swap! state-atom assoc
+           :first-midi-ts-micros timestamp
+           :first-epoch-ts-millis (current-epoch-millis)))
+
   (let [{:keys [command channel key velocity]} event-info]
     (println command event-info timestamp)
-    (case command
-      :on  (swap! state-atom update-in [:notes key] #((fnil conj []) % {:event event-info :ts timestamp}))
-      :off (swap! state-atom update-in [:notes key] #((fnil conj []) % {:event event-info :ts timestamp}))
-      :default)))
+    (swap! state-atom assoc :timestamp timestamp)
+    ;; FIXME: purge notes off older than some window back from timestamp
+    (when (#{:on :off} command)
+      (swap! state-atom update :notes (fnil conj []) {:event event-info :ts timestamp}))))
 
 (defn setup
   []
