c7443819e4b7cef85044e11912a246fd3c9d68a2
Sam Brauer <sam.brauer@gmail.com>
2023-02-06 17:39:11 -0500

    Add gil and demo
    gil is a lib for generating animated gif from quil

---
+++ src/hello_quil/gil_demo.clj
@@ -0,0 +1,25 @@
+(ns example
+  (:use quil.core)
+  (:use gil.core))
+
+(def θ (atom 0))
+
+(defn setup []
+  (smooth)
+  (ellipse-mode :center))
+
+(defn draw []
+  (background 0)
+  (translate 250 250)
+  (let [θ' (radians @θ)
+        x (* 150 (cos θ'))
+        y (* 150 (sin θ'))]
+    (ellipse x y 50 50))
+  (swap! θ + 20)
+  (save-animation "orbiting-circle.gif" 18 0))
+
+(sketch
+  :setup setup
+  :draw draw
+  :renderer :p2d
+  :size [500 500])
