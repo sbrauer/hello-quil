24e54b8cf8bc2186aa4692f25753b9a09d48f7ea
Sam Brauer <sam.brauer@gmail.com>
2023-02-08 16:39:47 -0500

    Another circle sketch

---
+++ src/hello_quil/circle.clj
@@ -0,0 +1,58 @@
+(ns hello-quil.core
+  (:require [quil.core :as q :include-macros true]))
+
+(def state-atom (atom nil))
+
+(def colors
+  [[255 126 116]   ;; orange
+   [ 0    0   0]   ;; black
+])
+
+(defn setup []
+  (q/background 0)                      ; black
+  (q/frame-rate 30)
+  (q/no-stroke)
+  (let [w (q/width)
+        h (q/height)]
+    (reset! state-atom
+            {:radius 1
+             :max-radius (Math/sqrt (+ (* w w) (* h h)))
+             :color-idx 0
+             :radius-step 10
+             :translation-vec [(/ w  2) (/ h 2)]})))
+
+(defn update-state! []
+  (let [{:keys [radius max-radius color-idx radius-step]} @state-atom]
+    (swap! state-atom update :radius #(+ % radius-step))
+    (when (> radius max-radius)
+      (swap! state-atom update :radius (constantly 1))
+      (swap! state-atom update :color-idx #(mod (inc %) (count colors)))
+      ;;(prn state-atom)
+      )))
+
+(defn draw []
+  (let [{:keys [radius radius-step color-idx translation-vec]} @state-atom]
+    (apply q/fill (nth colors color-idx))
+    (q/with-translation translation-vec
+      (q/ellipse 0 0 radius radius))
+    (update-state!)))
+
+(defn key-pressed []
+  (when-let [k (q/key-as-keyword)]
+    ;;(prinln "hey-pressed " k)
+    (case k
+      :left (swap! state-atom update :radius-step #(max 1 (dec %)))
+      :right (swap! state-atom update :radius-step inc)
+      ;; default expression to avoid "No matching clause" exception.
+      nil)
+    (prn @state-atom)))
+
+(q/defsketch hello-quil
+  :title "Hello World"
+  :size :fullscreen
+  ;;:size [1000 1000]
+  :setup setup
+  :draw draw
+  :features [:keep-on-top]
+  :renderer :p3d
+  :key-pressed key-pressed)
