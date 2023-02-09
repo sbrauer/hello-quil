580de5a0cc3110c92cc70c90f2eccab49d2de0da
Sam Brauer <sam.brauer@gmail.com>
2023-02-08 23:05:40 -0500

    Improve circle
    Speed and rand colors

--- src/hello_quil/circle.clj
+++ src/hello_quil/circle.clj
@@ -3,14 +3,12 @@
 
 (def state-atom (atom nil))
 
-(def colors
-  [[255 126 116]   ;; orange
-   [ 0    0   0]   ;; black
-])
+(def default-color [200 200 200])
+(def black [0 0 0])
 
 (defn setup []
-  (q/background 0)                      ; black
-  (q/frame-rate 30)
+  (apply q/background black)
+  (q/frame-rate 60)
   (q/no-stroke)
   (let [w (q/width)
         h (q/height)]
@@ -18,11 +16,12 @@
             {:radius 1
              :max-radius (Math/sqrt (+ (* w w) (* h h)))
              :color-idx 0
+             :colors [default-color black]
              :radius-step 10
              :translation-vec [(/ w  2) (/ h 2)]})))
 
 (defn update-state! []
-  (let [{:keys [radius max-radius color-idx radius-step]} @state-atom]
+  (let [{:keys [radius max-radius colors color-idx radius-step]} @state-atom]
     (swap! state-atom update :radius #(+ % radius-step))
     (when (> radius max-radius)
       (swap! state-atom update :radius (constantly 1))
@@ -31,18 +30,25 @@
       )))
 
 (defn draw []
-  (let [{:keys [radius radius-step color-idx translation-vec]} @state-atom]
+  (let [{:keys [radius radius-step colors color-idx translation-vec]} @state-atom]
     (apply q/fill (nth colors color-idx))
+    (apply q/background (nth colors (mod (inc color-idx) (count colors))))
     (q/with-translation translation-vec
       (q/ellipse 0 0 radius radius))
     (update-state!)))
 
+(defn add-rand-color! [state-atom]
+  (let [{:keys [colors color-idx]} @state-atom  ]
+    (swap! state-atom merge {:colors [(nth colors color-idx) (vec (repeatedly 3 #(int (rand 256))))]
+                             :color-idx 0})))
+
 (defn key-pressed []
   (when-let [k (q/key-as-keyword)]
-    ;;(prinln "hey-pressed " k)
+    ;;(prinln "key-pressed " k)
     (case k
-      :left (swap! state-atom update :radius-step #(max 1 (dec %)))
-      :right (swap! state-atom update :radius-step inc)
+      :left (swap! state-atom update :radius-step #(max 1 (- % 10)))
+      :right (swap! state-atom update :radius-step #(+ % 10))
+      :space (add-rand-color! state-atom)
       ;; default expression to avoid "No matching clause" exception.
       nil)
     (prn @state-atom)))
